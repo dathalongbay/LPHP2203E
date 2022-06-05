4d35964ecafbc5a3df212681a3689ecf5c4358ac
dat <dathalongbay@gmail.com>
2022-06-05 21:07:32 +0700

    111

---
+++ sourcecodes/ngay16/index16.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        vòng lặp trong php 
+        for , while ,do while giống js 
+        break trong vòng lặp giống js 
+        continue trong vòng lặp giống js
+    </pre>
+
+    <?php 
+    $a = 5;
+    $b = 25;
+    // in ra tổng các số từ $a đến $b 
+    // in ra tích các số từ $a đến $b 
+    ?>
+</body>
+</html>
