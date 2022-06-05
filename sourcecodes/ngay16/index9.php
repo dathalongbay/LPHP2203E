02b135fdb456e5492399986319abe02f0a8dae79
dat <dathalongbay@gmail.com>
2022-06-05 20:19:07 +0700

    111

---
+++ sourcecodes/ngay16/index9.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // dùng php in ra thẻ html 
+    echo "<div style=\"color:red\">thẻ div</div>";
+    ?>
+    <!-- dùng thẻ html lồng script php -->
+    <div><?php echo "thẻ div"; ?></div>
+</body>
+</html>
