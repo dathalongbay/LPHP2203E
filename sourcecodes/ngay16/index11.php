8a68c95852107d1dc65f3cfb33d78a6a5f6fdfee
dat <dathalongbay@gmail.com>
2022-06-05 20:43:36 +0700

    111

---
+++ sourcecodes/ngay16/index11.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+    if(condition){
+        // Code to be executed
+    }
+    </pre>
+
+    <?php
+    $age = 21; 
+    if ($age > 18) {
+        echo "<p>tuổi trưởng thành<p>";
+    }
+    
+    ?>
+</body>
+</html>
