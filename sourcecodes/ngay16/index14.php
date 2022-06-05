d6f859f69ac2dd10449a4ce5ddbda82a1fae8697
dat <dathalongbay@gmail.com>
2022-06-05 20:57:30 +0700

    111

---
+++ sourcecodes/ngay16/index14.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        điều kiện rút gọn
+        (điều kiện) ? "trả về khi điều kiện trả về true" : "trả về khi điều kiện trả về false";
+    </pre>
+
+    <?php 
+    $age = 5;
+    echo ($age < 18) ? 'trẻ em' : 'người lớn'; 
+    ?>
+
+
+</body>
+</html>
