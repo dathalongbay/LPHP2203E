58e746c1b594aaf774c76d419c6fef7d92d83fe1
dat <dathalongbay@gmail.com>
2022-06-07 20:11:47 +0700

    111

---
+++ sourcecodes/ngay17/index7.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    $target = [2,3,4,5,6];
+
+    // dùng vòng lặp để thay thế giá trị của các phần tử trong mảng trên
+    // thành dạng bình phương
+    // ví dụ 2 mũ 2 , 3 mũ 2 => [4,9,... ]
+
+    ?>
+
+</body>
+</html>
