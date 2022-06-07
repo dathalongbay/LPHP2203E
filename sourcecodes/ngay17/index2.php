56d8bc822db8853f2892b7a13c5c209e64cba45e
dat <dathalongbay@gmail.com>
2022-06-07 19:13:29 +0700

    111

--- sourcecodes/ngay17/index2.php
+++ sourcecodes/ngay17/index2.php
@@ -6,6 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <style>
+        table, td {
+            border: 1px solid red;
+        }
+    </style>
 </head>
 <body>
     <pre>
@@ -33,6 +39,19 @@
     $col = 8;
     $row = 20;
     // trong mỗi td in ra : hàng 1 cột 1 | hàng 1 cột 2
+
+    echo "<h1>table</h1>";
+    echo "<table>";
+        for($i = 1; $i <= $row; $i++) {
+            echo "<tr>";
+            for($j = 1; $j <= $col; $j++) {
+                echo "<td>Hàng $i cột $j</td>";
+            }
+            echo "</tr>";
+        }
+    echo "</table>";
     ?>
+
+
 </body>
 </html>
