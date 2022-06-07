f4ca3102b5f4c7cbff7839dd09f79fd440a886b1
dat <dathalongbay@gmail.com>
2022-06-07 19:00:43 +0700

    111

--- sourcecodes/ngay17/index2.php
+++ sourcecodes/ngay17/index2.php
@@ -1,1 +1,38 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        app tạo table html
+    </pre>
+
+    <table>
+        <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+    </table>
+    <?php
+    $col = 8;
+    $row = 20;
+    // trong mỗi td in ra : hàng 1 cột 1 | hàng 1 cột 2
+    ?>
+</body>
+</html>
