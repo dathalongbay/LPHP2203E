0fca4560a18effaeb56bb0b8630d8e6aa15b1bc6
dat <dathalongbay@gmail.com>
2022-06-12 19:05:29 +0700

    111

---
+++ sourcecodes/ngay18/bmi.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        xây dựng 1 form tính chỉ số bmi
+        nhập vào chiều cao và cân nặng in ra chỉ số bmi và phân lọai
+        sử dụng $_GET
+        tham khảo : https://hiu.vn/y-te-suc-khoe/cach-do-va-tinh-chi-so-bmi/#:~:text=BMI%20%3D%20(c%C3%A2n%20n%E1%BA%B7ng%20)%2F(chi%E1%BB%81u,c%C3%B3%20c%C3%A2n%20n%E1%BA%B7ng%20l%C3%BD%20t%C6%B0%E1%BB%9Fng.
+    </pre>
+
+</body>
+</html>
