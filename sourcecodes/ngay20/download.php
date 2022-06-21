58ce56c4900ff0d8f129d91bf105b01bd82a50a8
dat <dathalongbay@gmail.com>
2022-06-21 19:34:14 +0700

    111

---
+++ sourcecodes/ngay20/download.php
@@ -0,0 +1,31 @@
+<?php
+if(isset($_REQUEST["file"])){
+    // Get parameters
+    $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
+
+
+    /* Test whether the file name contains illegal characters
+    such as "../" using the regular expression */
+
+        $filepath = $file;
+
+
+        // Process download
+        if(file_exists($filepath)) {
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($filepath));
+            flush(); // Flush system output buffer
+            readfile($filepath);
+            die();
+        } else {
+            http_response_code(404);
+            die();
+        }
+
+}
+?>
