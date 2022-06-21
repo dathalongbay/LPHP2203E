a153f0bf6a50b2260e8de681441de9c7fd1f1486
dat <dathalongbay@gmail.com>
2022-06-21 19:24:16 +0700

    111

---
+++ sourcecodes/ngay20/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<div>
+    <a href="download/123.pdf" target="_blank">123.pdf</a>
+    <a href="download/123.zip" target="_blank">123.zip</a>
+    <a href="download/bg-grade-blue.png" target="_blank">bg-grade-blue.png</a>
+</div>
+
+</body>
+</html>
