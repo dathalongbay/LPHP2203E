57e6b507ca45154f9e3d7a3207f9730480a80a70
dat <dathalongbay@gmail.com>
2022-06-26 21:02:02 +0700

    111

---
+++ sourcecodes/ngay21/index2.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        https://www.w3schools.com/php/php_form_validation.asp
+        trim()
+        stripslashes()
+        htmlspecialchars()
+    </pre>
+
+</body>
+</html>
