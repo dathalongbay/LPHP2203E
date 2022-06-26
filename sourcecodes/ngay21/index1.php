bc7aa034f9b4ce9e3a6cd256f81992450fe754f9
dat <dathalongbay@gmail.com>
2022-06-26 20:13:45 +0700

    111

--- sourcecodes/ngay21/index1.php
+++ sourcecodes/ngay21/index1.php
@@ -20,6 +20,38 @@
     <?php
     if (!empty($_POST)) {
 
+        echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
+        $errors = [];
+        if (!isset($_POST['hoten'])) {
+            $errors['hoten'] = "Chưa nhập họ tên";
+        } else {
+            $regexHoten = '/^([a-z]{2,7}){0,1}( [a-z]{2,7}){1,4}$/';
+            $stringHoten = $_POST['hoten'];
+            if (!preg_match($regexHoten, $stringHoten)) {
+                $errors['hoten'] = "Họ tên không hợp lệ";
+            }
+        }
+
+        if (!isset($_POST['email'])) {
+            $errors['email'] = "Chưa nhập email";
+        } else {
+            $regexEmail = '/^[a-zA-Z0-9_\.]{5,32}@[a-zA-Z0-9_\.]{6,32}$/';
+            $stringEmail = $_POST['email'];
+            if (!preg_match($regexEmail, $stringEmail)) {
+                $errors['email'] = "email không hợp lệ";
+            }
+        }
+
+        if (count($errors)) {
+            // chuyên mảng errors sang chuỗi
+            $errorNotice = implode("<br>", $errors);
+            echo "<div style='color:red'>".$errorNotice."</div>";
+        } else {
+            echo "<div style='color:green'>Dữ liệu hợp lệ</div>";
+        }
+
     }
     ?>
 
@@ -28,32 +60,37 @@
         <form action="" name=dang_ky" method="post">
             <p>
                 <label>Họ tên *</label>
-                <span>Họ tên phải từ 2 từ đến 5 từ</span>
-                <input type="text" name="hoten" value="">
+                <span>Họ tên phải từ 2 từ đến 5 từ . tên là chữ không có số và không có ký tự đặc biệt như @ ! ...</span>
+                <input type="text" name="hoten" value="<?php echo isset($_POST['hoten']) ? $_POST['hoten'] : '' ?>">
+                ^([a-z]{2,7}){0,1}( [a-z]{2,7}){1,4}$
             </p>
 
             <p>
                 <label>Email *</label>
                 <span>tenEmail@domain ( từ 12 đến 64 ký tự)</span>
-                <input type="email" name="email" value="">
+                <input type="email" name="email" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>">
+                ^[a-zA-Z0-9_\.]{5,32}@[a-zA-Z0-9_\.]{6,32}$
             </p>
 
             <p>
                 <label>Số điện thoại *</label>
                 <span>Số phone từ 10 đến 11 số</span>
-                <input type="text" name="so_dien_thoai" value="">
+                <input type="text" name="so_dien_thoai" value="<?php echo isset($_POST['so_dien_thoai']) ? $_POST['so_dien_thoai'] : '' ?>">
+                ^[0-9]{10,11}$
             </p>
 
             <p>
                 <label>Sinh nhật</label>
                 <span>dd/mm/YYYY vd 02/10/1995</span>
-                <input type="text" name="sinh_nhat" value="">
+                <input type="text" name="sinh_nhat" value="<?php echo isset($_POST['sinh_nhat']) ? $_POST['sinh_nhat'] : '' ?>">
+                ^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$
             </p>
 
             <p>
                 <label>Ghi chú</label>
                 <span>Từ 8 đến 200 từ</span>
-                <textarea name="ghi_chu" id="ghi_chu" cols="30" rows="10"></textarea>
+                <textarea name="ghi_chu" id="ghi_chu" cols="30" rows="10"><?php echo isset($_POST['ghi_chu']) ? $_POST['ghi_chu'] : '' ?></textarea>
+                ^([a-z]{2,7}){0,1}( [a-z]{2,7}){1,199}$
             </p>
 
             <p>
