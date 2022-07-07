eeb30338aa1350291e05ea05bd30bd95f924b7dd
dat <dathalongbay@gmail.com>
2022-07-07 19:42:24 +0700

    111

--- sourcecodes/ngay23/index6.php
+++ sourcecodes/ngay23/index6.php
@@ -4,9 +4,30 @@ class Demo {
     // khai báo thuộc tính tĩnh static
     public static $a = 123;
 
+    public $b = 234;
+
     // khai báo method tĩnh
     public static function methodA(){
         echo "<br> methodA";
+        // truy cập đến thuộc tính tĩnh trong method tĩnh bằng từ khóa self
+        echo "<br> " . self::$a;
+        echo "<br> " . self::methodB();
+
+        // trong method tĩnh không được dùng $this
+        //echo "<br>" . $this->b;
+        // nếu muốn truy cập đến thuộc tính hay method thường
+        // khởi tạo object = cú pháp new self()
+        $self = new self();
+        echo "<br>" . $self->b;
+        echo "<br>" . $self->methodC();
+    }
+
+    public static function methodB() {
+        echo "<br> methodB";
+    }
+
+    public function methodC() {
+        echo "<br> methodC";
     }
 
 }
