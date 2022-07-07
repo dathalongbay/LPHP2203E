4542204860780b2be73a68d69d68375e3653af94
dat <dathalongbay@gmail.com>
2022-07-07 18:50:22 +0700

    111

---
+++ sourcecodes/ngay23/intex3.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Interface are similar to abstract classes. The difference between interfaces and abstract classes are:
+
+Interfaces cannot have properties, while abstract classes can
+All interface methods must be public, while abstract class methods is public or protected
+All methods in an interface are abstract, so they cannot be implemented in code and the abstract keyword is not necessary
+Classes can implement an interface while inheriting from another class at the same time
+ *
+ * Interface tương tự class trừu tượng . điểm khác biệt :
+ * 1 - interface không có thuộc tính , trong khi abstract class có thể có thuộc tính
+ * 2 - all method trong interface phải là public , trong khi class abstract là public hay protected
+ * 3 - classA extends abstractClassName implements interfaceName .
+ *
+ * => khi nào dùng abstract class và khi nào dùng interface .
+ */
