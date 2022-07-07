52de10faec6dddecbea80ee5940f19e89ee58e5d
dat <dathalongbay@gmail.com>
2022-07-07 20:31:52 +0700

    111

--- sourcecodes/ngay23/index8.php
+++ sourcecodes/ngay23/index8.php
@@ -1,13 +1,13 @@
 <?php
 namespace Dat;
-
-function demo() {
-    echo "<br> ABC";
-}
-
 class demoClass {
     public $a = 1;
     public function info() {
         echo "<br>". $this->a;
     }
 }
+function demo() {
+    echo "<br> ABC";
+}
+
+
--- sourcecodes/ngay23/index9.php
+++ sourcecodes/ngay23/index9.php
@@ -1,6 +1,13 @@
 <?php
 require_once "index8.php";
-//require_once "index8b.php";
-use Dat;
-$a = Dat\demoClass();
+require_once "index8b.php";
+
+// dùng từ khóa use để nạp namespace
+// dùng từ khóa as để tạo bí danh
+use \Dat\demoClass as demoClassA;
+use \TuanAnh\demoClass as demoClassB;
+
+$a = new demoClassA();
 $a->info();
+$b = new demoClassB();
+$b->info();
