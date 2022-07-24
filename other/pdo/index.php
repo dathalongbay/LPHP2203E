0044e376137c6afd878d0662ada2433576183b53
dat <dathalongbay@gmail.com>
2022-07-24 12:12:46 +0700

    111

---
+++ other/pdo/index.php
@@ -0,0 +1,33 @@
+<?php
+$host = '127.0.0.1';
+$db   = 'test';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$stmt = $pdo->query('SELECT name FROM users');
+while ($row = $stmt->fetch())
+{
+    echo $row['name'] . "\n";
+}
+
+$stmt = $pdo->prepare('SELECT * FROM users WHERE email = ? AND status=?');
+$stmt->execute([$email, $status]);
+$user = $stmt->fetch();
+// or
+$stmt = $pdo->prepare('SELECT * FROM users WHERE email = :email AND status=:status');
+$stmt->execute(['email' => $email, 'status' => $status]);
+$user = $stmt->fetch();
+
