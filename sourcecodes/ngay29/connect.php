793913d0bc86520b21ced0a0c7958d23a77b2dc8
dat <dathalongbay@gmail.com>
2022-07-28 18:55:41 +0700

    111

---
+++ sourcecodes/ngay29/connect.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost:3308'; // 127.0.0.1
+// port mặc định mysql : 3306
+$db = 'bookdb'; // tên cơ sở dữ liệu
+$user = 'root'; // username kết nối mysql
+$password = ''; // mật khẩu kết nối mysql
+
+$dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+
+    if ($pdo) {
+        echo "kết nối thành công !";
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
